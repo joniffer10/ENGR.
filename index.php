95424298f269090c41a43b0a6786e9ad5e937dea
Joniffer Mandac <101397206+joniffer10@users.noreply.github.com>
2023-08-04 18:39:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+  <title>ENGR.</title>
+  <link rel="icon" href="logo.jpg" type="image/icon type">
+
+</head>
+
+<body>
+  <link rel="stylesheet" href="menu.css">
+  <nav>
+    <a href="index.php" class="navlogo"><b>ENGR.</b></a>
+    <ul class="navlinks">
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">About Us</a></li>
+      <li><a href="#" class="Sign">Sign Up</a></li>
+    </ul>
+  </nav>
+  <style type="text/css">
+    body {
+      background-image: url('bgbg.jpg');
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-size: 100% 100%;
+    }
+  </style>
+  <link rel="stylesheet" href="switch.css">
+  <div class="wrapper">
+    <h2>ENGR. <span></span></h2>
+    <p>the environment in which we live.</p>
+    <a href="#">Make an Appointment</a>
+  </div>
+</body>
+
+</html>
